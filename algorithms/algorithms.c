0d2e20a88aaed44b66a3cacf9314be640d1bc4de
CLRNM <classroom.maksimow.nikita@gmail.com>
2024-06-18 05:30:47 +0300

    add find sum of maxes of pseudo diagonal function

--- algorithms/algorithms.c
+++ algorithms/algorithms.c
@@ -156,6 +156,32 @@ bool is_mutually_inverse_matrices(matrix m1, matrix m2) {
 
 
 // 7
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+
+long long int find_sum_of_maxes_of_pseudo_diagonal(matrix m) {
+    int n = m.n_rows;
+    int k = m.n_cols;
+    int* max_value = (int*) malloc(sizeof(int) * (n + k - 1));
+
+    for (int i = 0; i < n + k - 1; i++)
+        max_value[i] = (int)-1e18;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < k; j++)
+            if (i != j)
+                max_value[i - j + k - 1] = max(max_value[i - j + k - 1], m.values[i][j]);
+
+    long long int sum = 0;
+    for (int i = 0; i < n + k - 1; i++)
+        sum += max_value[i];
+
+    free(max_value);
+
+    return sum;
+}
 
 
 // 8
--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 
 int main() {
-    matrix a = create_matrix_from_array((int[]) {1, 2, 3,
-                                                 4, 5, 6,
-                                                 7, 8, 1}, 3, 3);
+    matrix a = create_matrix_from_array((int[]) {3, 2, 5, 4,
+                                                 1, 3, 6, 3,
+                                                 3, 2, 1, 2}, 3, 4);
 
-    position min_left = get_min_value_pos(a);
 
-    swap_penultimate_row(&a, min_left.col_index);
 
-    output_matrix(&a);
+    long long int s = find_sum_of_maxes_of_pseudo_diagonal(a);
+
+    printf("%lld", s);
 
     return 0;
 }
