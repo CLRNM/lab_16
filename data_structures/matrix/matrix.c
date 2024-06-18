4fc0d0c36cfc6c6ac10ad8062b2bcf0c530a70fd
CLRNM <classroom.maksimow.nikita@gmail.com>
2024-06-18 03:19:02 +0300

    first commit

---
+++ data_structures/matrix/matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by User on 18.06.2024.
+//
+
+#include "matrix.h"
---
+++ data_structures/matrix/matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 18.06.2024.
+//
+
+#ifndef LAB_16_MATRIX_H
+#define LAB_16_MATRIX_H
+
+#endif //LAB_16_MATRIX_H
